4c2f7b5c89be9c851f18891b10fe21be7858015b
Jelmer Berghs <jelmer.berghs@outlook.com>
2019-11-13 14:16:17 +0100

    unit test added

---
+++ src/test/java/ucll/project/domain/user/SampleTest.java
@@ -0,0 +1,18 @@
+package ucll.project.domain.user;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * This is a sample unit test, write your own!
+ */
+public class SampleTest {
+
+    @Test
+    public void Unit_test_test() {
+
+        assertEquals(true, true);
+    }
+
+}
